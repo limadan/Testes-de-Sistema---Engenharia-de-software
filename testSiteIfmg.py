b8578d16b77deb828a08104fdde2e34a008cb215
limadan <dbatistalima135@gmail.com>
2024-09-02 20:03:55 -0300

    teste de busca

--- testSiteIfmg.py
+++ testSiteIfmg.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -82,16 +83,33 @@ class TestSiteIfmg(unittest.TestCase):
     def test_acessoaSistemas(self):
         self.driver.get("https://www.ifmg.edu.br/sabara")
 
-        linkacessoaSistemas = WebDriverWait(self.driver, 20).until(
+        linkAcessoASistemas = WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[1]/div[2]/ul/li[6]/a"))
         )
-        linkacessoaSistemas.click()
+        linkAcessoASistemas.click()
 
         WebDriverWait(self.driver, 10).until(
             EC.title_contains("Acesso a Sistemas — Instituto Federal de Educação, Ciência e Tecnologia de Minas Gerais IFMG")
         )
 
         self.assertIn("Acesso a Sistemas — Instituto Federal de Educação, Ciência e Tecnologia de Minas Gerais IFMG", self.driver.title)
+
+    def test_busca(self):
+        self.driver.get("https://www.ifmg.edu.br/sabara")
+
+        inputBusca = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[1]/div[1]/div[2]/form/fieldset/input[1]"))
+        )
+        inputBusca.send_keys("Teste")
+        inputBusca.send_keys(Keys.ENTER)
+
+        WebDriverWait(self.driver, 10).until(
+            EC.title_contains("Busca — Instituto Federal de Educação, Ciência e Tecnologia de Minas Gerais Campus Sabará")
+        )
+
+        spanBusca = self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div/form/div[1]/div[1]/h1/strong/span")
+
+        self.assertIn("Teste", spanBusca.text)
 
     def tearDown(self):
         # Encerrando o navegador
